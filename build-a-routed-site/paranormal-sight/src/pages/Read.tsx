53c26ef9529222eef5111118ae24f49ff14859ad
Nícolas <nicolasbretschneider1@gmail.com>
2025-12-29 10:24:48 -0300

    Feat: cria variáveis

--- build-a-routed-site/paranormal-sight/src/pages/Read.tsx
+++ build-a-routed-site/paranormal-sight/src/pages/Read.tsx
@@ -3,7 +3,7 @@ import styles from './Read.module.css'
 export function Read() {
     return (
         <main className={styles.main}>
-            <h1 className={styles.h1}>BBBBBB</h1>
+            <h1 className={styles.h1}>SIGHTINGS</h1>
         </main>
     )
 }
